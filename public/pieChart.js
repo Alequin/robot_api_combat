ee76d13d76ac917d1c0f4771a2c2b2a768bda21e
James Cox <j-cox03@hotmail.com>
2017-08-04 23:43:41 +0100

    Removed custom colour from pie chart

--- public/pieChart.js
+++ public/pieChart.js
@@ -3,7 +3,7 @@ function PieChart(container, countries){
 
   var dataSet = [];
   for(var country of countries){
-    dataSet.push({name: country.name, y: country.score.win, colour: "blue"});
+    dataSet.push({name: country.name, y: country.score.win});
   }
 
   var chart = new Highcharts.Chart({
