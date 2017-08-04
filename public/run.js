045a6fbe163d94eca46009e1c5cb7b287e1ad267
James Cox <j-cox03@hotmail.com>
2017-08-04 11:03:26 +0100

    Made javascript request for countries

--- public/run.js
+++ public/run.js
@@ -1,5 +1,16 @@
+function makeRequest(url, onRequest){
+  var request = new XMLHttpRequest();
+  request.addEventListener("load", onRequest);
+  request.open("GET", url);
+  request.send();
+}
+
+function onRequest(){
+  var jsonString = this.responseText;
+  var countries = JSON.parse(jsonString);
+  console.log(countries);
+}
+
 window.addEventListener("load", function(){
-
-
-
-})
+  // makeRequest("https://restcountries.eu/rest/v2/all", onRequest);
+});
