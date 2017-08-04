dafbd5ac4c60dd815e69c8a1d5e718e7af7f3f11
James Cox <j-cox03@hotmail.com>
2017-08-04 13:43:44 +0100

    Made pie chart section work

---
+++ public/pieChart.js
@@ -0,0 +1,16 @@
+
+function PieChart(){
+  var container = document.querySelector("#pie-chart");
+
+  var chart = new Highcharts.Chart({
+    chart: {type: "pie", renderTo: container},
+    title: {text: "Pokemon Types I've Caught"},
+    series: [{
+      name: "Type", data: [
+        {name: "Fire", y: 75, color: "#ffac33"},
+        {name: "Water", y: 25, color: "#0000ff"},
+        {name: "Grass", y: 1, color: "#00ff00"}
+      ]
+    }],
+  });
+}
