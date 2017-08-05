ebd37ab8a9b78d56f3d0d66788b133e7ad02b166
James Cox <j-cox03@hotmail.com>
2017-08-05 10:14:20 +0100

    Made method to produce fake score data

--- public/run.js
+++ public/run.js
@@ -8,4 +8,15 @@ window.addEventListener("load", function(){
 
   testingbuildCountriesList();
   // buildCountriesList();
+
+  produceFakeScoreData();
 });
+
+function produceFakeScoreData(){
+
+  for(var country of allCountries){
+    country.score.win = dice(1, 100);
+    country.score.loss = dice(1, 100);
+  }
+
+}
